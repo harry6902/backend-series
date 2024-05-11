3290420573d426e4d5dee5da7b961ae010960295
harry6902 <manepallihari2002@gmail.com>
2024-05-11 13:36:29 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import { dbConnect } from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
 dbConnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+console.log("MONGO DB Connection failed !!!", err)
+})
 
