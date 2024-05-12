346fb71b3f0ba082075e6c1ab9980bbceb22069d
harry6902 <manepallihari2002@gmail.com>
2024-05-12 13:51:56 +0530

    setup completed

--- src/app.js
+++ src/app.js
@@ -15,5 +15,14 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.get('/',(req,res)=>
+{
+    res.send('Hello harry')
+})
+
+//import routes
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
 
 export {app}
